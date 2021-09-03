278ec18ecac73489fe0878970b5f02af5ab2fec7
Marcos Salas <marcosrorai@gmail.com>
2021-09-03 15:40:11 -0500

    Excepciones basicas en clases de lectura

--- src/turismoEnLaTierraMedia/LeerArchivoCrearUsuariosYCrearLista.java
+++ src/turismoEnLaTierraMedia/LeerArchivoCrearUsuariosYCrearLista.java
@@ -2,6 +2,7 @@ package turismoEnLaTierraMedia;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -15,23 +16,62 @@ public class LeerArchivoCrearUsuariosYCrearLista {
 			sc = new Scanner(new File(archivo));
 
 			while (sc.hasNext()) {
-				String linea = sc.nextLine();
-				usuarios.add(crearUsuario(linea));
+				try {
+					String linea = sc.nextLine();
+					usuarios.add(crearUsuario(linea));
+				} catch (ArrayIndexOutOfBoundsException e) {
+					System.out.println(e.getMessage());
+				}
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} finally {
+			try {
+				if (sc != null) {
+					sc.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
 		}
-		sc.close();
 
 		return usuarios;
 	}
 
-	public static Usuario crearUsuario(String linea) {
+	public static Usuario crearUsuario(String linea) throws Exception, ArrayIndexOutOfBoundsException {
 		String datos[] = linea.split(",");
-		String nombre = datos[0];
-		double presupuesto = Double.parseDouble(datos[1]);
-		double tiempoDisponible = Double.parseDouble(datos[2]);
-		TipoDeAtraccion preferencia = TipoDeAtraccion.valueOf(datos[3]);
-		return new Usuario(nombre, presupuesto, tiempoDisponible, preferencia);
+		Usuario usuario = null;
+		String nombre = null;
+		double presupuesto = 0;
+		double tiempoDisponible = 0;
+		TipoDeAtraccion preferencia = null;
+		if (datos.length < 4) {
+			throw new ArrayIndexOutOfBoundsException("faltan parámetros");
+		} else if (datos.length > 4) {
+			throw new ArrayIndexOutOfBoundsException("sobran parámetros");
+
+		}
+
+		try {
+			nombre = datos[0];
+			presupuesto = Double.parseDouble(datos[1]);
+			tiempoDisponible = Double.parseDouble(datos[2]);
+			preferencia = TipoDeAtraccion.valueOf(datos[3].toUpperCase());
+
+			usuario = new Usuario(nombre, presupuesto, tiempoDisponible, preferencia);
+
+		} catch (Exception e) {
+			throw new Exception("Parámetro inválido");
+
+		}
+		return usuario;
+
 	}
 }
