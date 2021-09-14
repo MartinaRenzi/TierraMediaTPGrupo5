72b02d66681639e0fdc7472fdd5539bfcf86258b
Marcos Salas <marcosrorai@gmail.com>
2021-09-13 21:09:15 -0500

    creacion de parque

--- src/turismoEnLaTierraMedia/App.java
+++ src/turismoEnLaTierraMedia/App.java
@@ -5,41 +5,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
-
-	private static List<Usuario> usuarios = new LinkedList<Usuario>();
-	private static List<Producto> productos = new LinkedList<Producto>();
-
+	
+	
 	public static void main(String[] args) throws Exception {
-		usuarios = LeerArchivoCrearUsuariosYCrearLista.getUsuarios("archivos/usuarios.csv");
-		// System.out.print(usuarios);//para probarlo
-
-		List<Atraccion> atracciones = LeerArchivoCrearAtraccionesYCrearLista.getAtracciones("archivos/atracciones.csv");
-		List<Promocion> promociones = LeerArchivoCrearPromocionesyCrearLista.getPromociones("archivos/promociones.csv",
-				atracciones);
-		for (Producto atraccion : atracciones) {
-			productos.add(atraccion);
-		}
-		for (Producto promocion : promociones) {
-			productos.add(promocion);
-		}
-		//System.out.println(productos);// para probarlo
-		
-		
-		for (Usuario usuario:usuarios) {
-			productos.sort(new ProductosPorPreferencia(usuario.getPreferencia()));
-			System.out.println(usuario);
-			
-			for (Producto oferta:productos) {
-				Scanner aceptacionOferta = new Scanner(System.in);
-				System.out.println("ingrese S para aceptar y N para seguir viendo nuestras Atracciones");
-				System.out.println(oferta);
-				String respuesta = aceptacionOferta.next();
-				
-			}
-			
-			
-		}
-		
-	}
-
+        Parque parque= new Parque();
+       
+        parque.ofrecerProductos();
+    }
 }
