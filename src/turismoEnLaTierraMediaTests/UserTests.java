cee40c2af48715a682d78bd2e2432af03553f727
Martina Renzi <martinasrenzi@gmail.com>
2021-10-29 19:34:47 -0300

    Versión final parte B

--- src/turismoEnLaTierraMediaTests/UserTests.java
+++ src/turismoEnLaTierraMediaTests/UserTests.java
@@ -1,24 +1,37 @@
 package turismoEnLaTierraMediaTests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.sql.SQLException;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import dao.UsuarioDAOImpl;
 import turismoEnLaTierraMedia.Atraccion;
+import turismoEnLaTierraMedia.Producto;
 import turismoEnLaTierraMedia.TipoDeAtraccion;
 import turismoEnLaTierraMedia.Usuario;
 
 public class UserTests {
 
-	Atraccion Moria;
-	Usuario Eowyn;
+	Atraccion Moria, Lothlorien;
+	Usuario Eowyn, Gandalf;
+	int nuevoPresupuesto = 200;
+	double nuevoTiempo = 8;
+	List<Producto> productos;
+	
 
 	@Before
 	public void crear() {
 		Eowyn = new Usuario("Eowyn", 10, 8, TipoDeAtraccion.AVENTURA, 1);
+		
+		Gandalf = new Usuario("Gandalf", nuevoPresupuesto, nuevoTiempo, TipoDeAtraccion.PAISAJE, 1);
 
 		Moria = new Atraccion("Moria", 10, 2, TipoDeAtraccion.AVENTURA, 6, 1);
+		
+		Lothlorien = new Atraccion("Lothlorien", 35, 1, TipoDeAtraccion.DEGUSTACION, 26, 7);
 	}
 
 	@Test
@@ -39,6 +52,14 @@ public class UserTests {
 		double tiempoDesponibleEsp = 6;
 		Eowyn.descontarTiempo(Moria);
 		assertEquals(tiempoDesponibleEsp, Eowyn.getTiempoDisponible(), 0);
+	}
+	
+	
+	@Test 
+	public void actualizarPresupuestoYTiempoUsuario() throws SQLException {
+		UsuarioDAOImpl usuario = new UsuarioDAOImpl();
+		usuario.update(Gandalf); 
+		assertEquals(nuevoPresupuesto, Gandalf.getPresupuesto(), 0);
 	}
 
 }
